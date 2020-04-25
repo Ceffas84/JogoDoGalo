f88891cfd9928e8aa59155a7bf0bfdbd60965d80
Ricardo <2190787@my.ipleiria.pt>
2020-04-25 18:02:23 +0100

    Atualização ClienteForm

--- JogoDoGalo/ClientForm.Designer.cs
+++ JogoDoGalo/ClientForm.Designer.cs
@@ -37,258 +37,174 @@
             this.bt_EnviaMensagem = new System.Windows.Forms.Button();
             this.tb_EscreveMensagem = new System.Windows.Forms.TextBox();
             this.lb_Mensagens = new System.Windows.Forms.ListBox();
-            this.bt_Atualizar = new System.Windows.Forms.Button();
+            this.bt_Autenticar = new System.Windows.Forms.Button();
             this.tb_Password = new System.Windows.Forms.TextBox();
             this.tb_Jogador = new System.Windows.Forms.TextBox();
             this.tb_Servidor = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.bt_Celula9 = new System.Windows.Forms.Button();
-            this.bt_Celula7 = new System.Windows.Forms.Button();
-            this.bt_Celula8 = new System.Windows.Forms.Button();
-            this.bt_Celula3 = new System.Windows.Forms.Button();
-            this.bt_Celula5 = new System.Windows.Forms.Button();
-            this.bt_Celula6 = new System.Windows.Forms.Button();
-            this.bt_Celula4 = new System.Windows.Forms.Button();
-            this.bt_Celula1 = new System.Windows.Forms.Button();
-            this.bt_Celula2 = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(164, 450);
+            this.button1.Location = new System.Drawing.Point(79, 402);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 34;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(399, 378);
+            this.label8.Location = new System.Drawing.Point(255, 343);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(61, 17);
+            this.label8.Size = new System.Drawing.Size(46, 13);
             this.label8.TabIndex = 33;
             this.label8.Text = "Servidor";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(291, 393);
+            this.label6.Location = new System.Drawing.Point(174, 355);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(56, 17);
+            this.label6.Size = new System.Drawing.Size(43, 13);
             this.label6.TabIndex = 32;
             this.label6.Text = "aaaaaa";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(161, 393);
+            this.label7.Location = new System.Drawing.Point(77, 355);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(56, 17);
+            this.label7.Size = new System.Drawing.Size(43, 13);
             this.label7.TabIndex = 31;
             this.label7.Text = "aaaaaa";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(291, 356);
+            this.label5.Location = new System.Drawing.Point(174, 325);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(72, 17);
+            this.label5.Size = new System.Drawing.Size(54, 13);
             this.label5.TabIndex = 30;
             this.label5.Text = "Jogador 2";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(161, 356);
+            this.label4.Location = new System.Drawing.Point(77, 325);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(72, 17);
+            this.label4.Size = new System.Drawing.Size(54, 13);
             this.label4.TabIndex = 29;
             this.label4.Text = "Jogador 1";
             // 
             // bt_EnviaMensagem
             // 
-            this.bt_EnviaMensagem.Location = new System.Drawing.Point(642, 475);
+            this.bt_EnviaMensagem.Location = new System.Drawing.Point(482, 386);
+            this.bt_EnviaMensagem.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bt_EnviaMensagem.Name = "bt_EnviaMensagem";
-            this.bt_EnviaMensagem.Size = new System.Drawing.Size(314, 23);
+            this.bt_EnviaMensagem.Size = new System.Drawing.Size(236, 19);
             this.bt_EnviaMensagem.TabIndex = 28;
             this.bt_EnviaMensagem.Text = "Enviar Mensagem";
             this.bt_EnviaMensagem.UseVisualStyleBackColor = true;
             // 
             // tb_EscreveMensagem
             // 
-            this.tb_EscreveMensagem.Location = new System.Drawing.Point(642, 393);
+            this.tb_EscreveMensagem.Location = new System.Drawing.Point(482, 319);
+            this.tb_EscreveMensagem.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tb_EscreveMensagem.Multiline = true;
             this.tb_EscreveMensagem.Name = "tb_EscreveMensagem";
-            this.tb_EscreveMensagem.Size = new System.Drawing.Size(314, 45);
+            this.tb_EscreveMensagem.Size = new System.Drawing.Size(236, 37);
             this.tb_EscreveMensagem.TabIndex = 27;
             this.tb_EscreveMensagem.Text = "Escreva sua mensagem";
             // 
             // lb_Mensagens
             // 
             this.lb_Mensagens.FormattingEnabled = true;
-            this.lb_Mensagens.ItemHeight = 16;
-            this.lb_Mensagens.Location = new System.Drawing.Point(642, 257);
+            this.lb_Mensagens.Location = new System.Drawing.Point(482, 209);
+            this.lb_Mensagens.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lb_Mensagens.Name = "lb_Mensagens";
-            this.lb_Mensagens.Size = new System.Drawing.Size(314, 116);
+            this.lb_Mensagens.Size = new System.Drawing.Size(236, 95);
             this.lb_Mensagens.TabIndex = 26;
             // 
-            // bt_Atualizar
+            // bt_Autenticar
             // 
-            this.bt_Atualizar.Location = new System.Drawing.Point(642, 210);
-            this.bt_Atualizar.Name = "bt_Atualizar";
-            this.bt_Atualizar.Size = new System.Drawing.Size(314, 23);
-            this.bt_Atualizar.TabIndex = 25;
-            this.bt_Atualizar.Text = "Atualizar";
-            this.bt_Atualizar.UseVisualStyleBackColor = true;
+            this.bt_Autenticar.Location = new System.Drawing.Point(482, 171);
+            this.bt_Autenticar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bt_Autenticar.Name = "bt_Autenticar";
+            this.bt_Autenticar.Size = new System.Drawing.Size(236, 19);
+            this.bt_Autenticar.TabIndex = 25;
+            this.bt_Autenticar.Text = "Atualizar";
+            this.bt_Autenticar.UseVisualStyleBackColor = true;
             // 
             // tb_Password
             // 
-            this.tb_Password.Location = new System.Drawing.Point(727, 151);
+            this.tb_Password.Location = new System.Drawing.Point(545, 123);
+            this.tb_Password.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tb_Password.Name = "tb_Password";
-            this.tb_Password.Size = new System.Drawing.Size(229, 22);
+            this.tb_Password.Size = new System.Drawing.Size(173, 20);
             this.tb_Password.TabIndex = 24;
             // 
             // tb_Jogador
             // 
-            this.tb_Jogador.Location = new System.Drawing.Point(727, 107);
+            this.tb_Jogador.Location = new System.Drawing.Point(545, 87);
+            this.tb_Jogador.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tb_Jogador.Name = "tb_Jogador";
-            this.tb_Jogador.Size = new System.Drawing.Size(229, 22);
+            this.tb_Jogador.Size = new System.Drawing.Size(173, 20);
             this.tb_Jogador.TabIndex = 23;
             // 
             // tb_Servidor
             // 
-            this.tb_Servidor.Location = new System.Drawing.Point(727, 56);
+            this.tb_Servidor.Location = new System.Drawing.Point(545, 46);
+            this.tb_Servidor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tb_Servidor.Name = "tb_Servidor";
-            this.tb_Servidor.Size = new System.Drawing.Size(229, 22);
+            this.tb_Servidor.Size = new System.Drawing.Size(173, 20);
             this.tb_Servidor.TabIndex = 22;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(639, 154);
+            this.label3.Location = new System.Drawing.Point(479, 125);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 21;
             this.label3.Text = "Password";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(639, 107);
+            this.label2.Location = new System.Drawing.Point(479, 87);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 17);
+            this.label2.Size = new System.Drawing.Size(45, 13);
             this.label2.TabIndex = 20;
             this.label2.Text = "Jogador";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(639, 56);
+            this.label1.Location = new System.Drawing.Point(479, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 19;
             this.label1.Text = "Servidor";
             // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel1.Controls.Add(this.bt_Celula9);
-            this.panel1.Controls.Add(this.bt_Celula7);
-            this.panel1.Controls.Add(this.bt_Celula8);
-            this.panel1.Controls.Add(this.bt_Celula3);
-            this.panel1.Controls.Add(this.bt_Celula5);
-            this.panel1.Controls.Add(this.bt_Celula6);
-            this.panel1.Controls.Add(this.bt_Celula4);
-            this.panel1.Controls.Add(this.bt_Celula1);
-            this.panel1.Controls.Add(this.bt_Celula2);
-            this.panel1.Location = new System.Drawing.Point(111, 56);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(433, 267);
-            this.panel1.TabIndex = 18;
-            // 
-            // bt_Celula9
-            // 
-            this.bt_Celula9.Location = new System.Drawing.Point(272, 169);
-            this.bt_Celula9.Name = "bt_Celula9";
-            this.bt_Celula9.Size = new System.Drawing.Size(65, 65);
-            this.bt_Celula9.TabIndex = 16;
-            this.bt_Celula9.UseVisualStyleBackColor = true;
-            // 
-            // bt_Celula7
-            // 
-            this.bt_Celula7.Location = new System.Drawing.Point(94, 169);
-            this.bt_Celula7.Name = "bt_Celula7";
-            this.bt_Celula7.Size = new System.Drawing.Size(65, 65);
-            this.bt_Celula7.TabIndex = 15;
-            this.bt_Celula7.UseVisualStyleBackColor = true;
-            // 
-            // bt_Celula8
-            // 
-            this.bt_Celula8.Location = new System.Drawing.Point(183, 169);
-            this.bt_Celula8.Name = "bt_Celula8";
-            this.bt_Celula8.Size = new System.Drawing.Size(65, 65);
-            this.bt_Celula8.TabIndex = 14;
-            this.bt_Celula8.UseVisualStyleBackColor = true;
-            // 
-            // bt_Celula3
-            // 
-            this.bt_Celula3.Location = new System.Drawing.Point(272, 27);
-            this.bt_Celula3.Name = "bt_Celula3";
-            this.bt_Celula3.Size = new System.Drawing.Size(65, 65);
-            this.bt_Celula3.TabIndex = 13;
-            this.bt_Celula3.UseVisualStyleBackColor = true;
-            // 
-            // bt_Celula5
-            // 
-            this.bt_Celula5.Location = new System.Drawing.Point(183, 98);
-            this.bt_Celula5.Name = "bt_Celula5";
-            this.bt_Celula5.Size = new System.Drawing.Size(65, 65);
-            this.bt_Celula5.TabIndex = 12;
-            this.bt_Celula5.UseVisualStyleBackColor = true;
-            // 
-            // bt_Celula6
-            // 
-            this.bt_Celula6.Location = new System.Drawing.Point(272, 98);
-            this.bt_Celula6.Name = "bt_Celula6";
-            this.bt_Celula6.Size = new System.Drawing.Size(65, 65);
-            this.bt_Celula6.TabIndex = 11;
-            this.bt_Celula6.UseVisualStyleBackColor = true;
-            // 
-            // bt_Celula4
-            // 
-            this.bt_Celula4.Location = new System.Drawing.Point(94, 98);
-            this.bt_Celula4.Name = "bt_Celula4";
-            this.bt_Celula4.Size = new System.Drawing.Size(65, 65);
-            this.bt_Celula4.TabIndex = 10;
-            this.bt_Celula4.UseVisualStyleBackColor = true;
-            // 
-            // bt_Celula1
-            // 
-            this.bt_Celula1.Location = new System.Drawing.Point(94, 27);
-            this.bt_Celula1.Name = "bt_Celula1";
-            this.bt_Celula1.Size = new System.Drawing.Size(65, 65);
-            this.bt_Celula1.TabIndex = 9;
-            this.bt_Celula1.UseVisualStyleBackColor = true;
-            // 
-            // bt_Celula2
-            // 
-            this.bt_Celula2.Location = new System.Drawing.Point(183, 27);
-            this.bt_Celula2.Name = "bt_Celula2";
-            this.bt_Celula2.Size = new System.Drawing.Size(65, 65);
-            this.bt_Celula2.TabIndex = 1;
-            this.bt_Celula2.UseVisualStyleBackColor = true;
-            // 
             // ClientForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label6);
@@ -298,18 +214,15 @@
             this.Controls.Add(this.bt_EnviaMensagem);
             this.Controls.Add(this.tb_EscreveMensagem);
             this.Controls.Add(this.lb_Mensagens);
-            this.Controls.Add(this.bt_Atualizar);
+            this.Controls.Add(this.bt_Autenticar);
             this.Controls.Add(this.tb_Password);
             this.Controls.Add(this.tb_Jogador);
             this.Controls.Add(this.tb_Servidor);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ClientForm";
             this.Text = "Jogo do Galo - Cliente";
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -326,23 +239,13 @@
         private System.Windows.Forms.Button bt_EnviaMensagem;
         private System.Windows.Forms.TextBox tb_EscreveMensagem;
         private System.Windows.Forms.ListBox lb_Mensagens;
-        private System.Windows.Forms.Button bt_Atualizar;
+        private System.Windows.Forms.Button bt_Autenticar;
         private System.Windows.Forms.TextBox tb_Password;
         private System.Windows.Forms.TextBox tb_Jogador;
         private System.Windows.Forms.TextBox tb_Servidor;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button bt_Celula9;
-        private System.Windows.Forms.Button bt_Celula7;
-        private System.Windows.Forms.Button bt_Celula8;
-        private System.Windows.Forms.Button bt_Celula3;
-        private System.Windows.Forms.Button bt_Celula5;
-        private System.Windows.Forms.Button bt_Celula6;
-        private System.Windows.Forms.Button bt_Celula4;
-        private System.Windows.Forms.Button bt_Celula1;
-        private System.Windows.Forms.Button bt_Celula2;
     }
 }
 
---
+++ JogoDoGalo/Models/Jogador.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDoGalo_Server.Models
+{
+    class Jogador
+    {
+        public int IdJogador;
+        public string Nome;
+        public char Simbolo;
+        public int NumJogadas;
+        public Jogador()
+        {
+        }
+        public Jogador(string nome, char simbolo)
+        {
+            this.IdJogador = new int();
+            this.Nome = nome;
+            this.NumJogadas = 0;
+            this.Simbolo = simbolo;
+        }
+    }
+}
