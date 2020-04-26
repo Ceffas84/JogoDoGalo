c6f754bf445e047c19eba13cd7dab0e22a2525e5
Ricardo <2190787@my.ipleiria.pt>
2020-04-26 15:47:31 +0100

    Class Player (para armazenamento dos players no servidor)

---
+++ JogoDoGalo_Server/Models/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDoGalo_Server.Models
+{
+    class Player
+    {
+        public TcpClient TcpClient { get; set; }
+        public byte[] PublicKey { get; set; }
+        public byte[] Salt { get; set; }
+        public byte[] HashUsername { set; get; }
+        public byte[] HashPassword { set; get; }
+        Player()
+        {
+
+        }
+    }
+}
