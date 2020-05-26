9b8ffb17fcd34678c6c4324710909a268f3d2c03
Simão Pedro <simao.s.pedro@gmail.com>
2020-05-26 11:04:10 +0100

    Alteração do enum ServerResponse

--- JogoDoGalo_Server/Models/ServerResponse.cs
+++ JogoDoGalo_Server/Models/ServerResponse.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace JogoDoGalo_Server.Models
 {
-    class ServerResponse
+    public enum ServerResponse
     {
+        LOGIN_ERROR = 00,
+        REGISTER_ERROR = 01,
+        NOT_LOGGED = 02,
+        LOGIN_SUCCESS = 10,
+        REGISTER_SUCCESS = 11,
+        NOT_ENOUGH_PLAYERS = 30
     }
 }
