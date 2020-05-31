b3705628f4d33860c2d3c660706765d1ff8ad76d
Ricardo <2190787@my.ipleiria.pt>
2020-05-31 22:48:32 +0100

    Class Lobby
    Função para introdução de clientes no lobby (devolve o id do novo cliente)

--- JogoDoGaloV1.0/Server/Models/Lobby.cs
+++ JogoDoGaloV1.0/Server/Models/Lobby.cs
@@ -16,5 +16,23 @@ namespace Server.Models
             listClients = new List<Client>();
             gameRoom = new GameRoom();
         }
+        public int AddClient(Client client)
+        {
+            listClients.Add(client);
+
+            int lastClientID = 0;
+            foreach(Client cli in listClients)
+            {
+                if(cli.ClientID > lastClientID)
+                {
+                    lastClientID = cli.ClientID;
+                }
+            }
+            return lastClientID + 1;
+        }
+        public void RemoveClient(Client client)
+        {
+            listClients.Remove(client);
+        }
     }
 }
