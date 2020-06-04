76d27f44107058d655d1496fbfa9c97645b362b1
Simão Pedro <simao.s.pedro@gmail.com>
2020-06-04 23:18:03 +0100

    Enum TypeGameOver comentado.

--- VersaoFinal/JogoDoGaloV1.0/Server/Models/TypeGameOver.cs
+++ VersaoFinal/JogoDoGaloV1.0/Server/Models/TypeGameOver.cs
@@ -1,8 +1,11 @@
-﻿
-
-
-namespace Server.Models
+﻿namespace Server.Models
 {
+    /**
+     * <summary>    Values that represent types of game over. 
+     *              Enumeração que representa os tipos de game over possíveis. </summary>
+     *
+     * <remarks>    Simão Pedro, 04/06/2020. </remarks>
+     */
     public enum TypeGameOver
     {
         Draw,
